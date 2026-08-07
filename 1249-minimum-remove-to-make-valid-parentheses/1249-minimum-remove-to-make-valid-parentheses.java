112aa62a311d5fe610dfc08f1bfc7293203f66ce
Ankit Saini <ankiitsaini1403@gmail.com>
2026-08-07 17:57:42 +0530

    Time: 17 ms (78.09%), Space: 47.1 MB (80.95%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,34 +1,41 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         
-        char arr[] = s.toCharArray();
-        Stack<Integer> stack = new Stack<>();
+        int open = 0 ;
+          StringBuilder first = new StringBuilder();
+         for(char ch : s.toCharArray()){
+            if(ch =='('){
+                 open++;
+                 first.append(ch);
+            }
+            else if( ch == ')'){
+                   if(open>0){
+                       open--;
+                       first.append(ch);
+                   }
+            }else {
+                   first.append(ch);
+            }
+         }
 
-        for(int i = 0 ; i<arr.length ; i++){
-               
-               if(arr[i] == '('){
-                   stack.push(i);
-               }else if(arr[i]==')'){
-                     if(!stack.isEmpty()){
-                         stack.pop();
-                     }
-                     else {
-                           arr[i] = '#';
-                     }
+  StringBuilder ans = new StringBuilder();
+  int close = 0 ;
+  for(int i = first.length()-1 ; i>= 0 ;i--){
+          
+          char ch = first.charAt(i);
+          if(ch==')'){
+             close++;
+             ans.append(ch);
+          }else if(ch=='('){
+               if(close>0){
+                  close--;
+                  ans.append(ch);
                }
+          }else {
+               ans.append(ch);
+          }
+  }
+    return ans.reverse().toString();
+
         }
-        while(!stack.isEmpty()){
-                 arr[stack.pop()] ='#';
-               }
-    
-       StringBuilder ans = new StringBuilder();
-
-               for(char ch : arr){
-                 if(ch !='#'){
-                      ans.append(ch);
-                 }
-               }
-                
-              return ans.toString();
     }
-}
