b4c90f33af618fc7e0d8c5b40a2d96aa6d4d79ee
Ankit Saini <ankiitsaini1403@gmail.com>
2026-08-19 06:37:16 +0530

    Time: 1 ms (99.96%), Space: 43.5 MB (76.48%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,27 +1,28 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] lastIndex = new int[26];
-        boolean[] visited = new boolean[26];
-        for(int i = 0 ;i<s.length();i++){
-             lastIndex[s.charAt(i)-'a'] = i ;
-        }
+        int  lastIndex[] = new int[26];
+         boolean visited[] = new boolean[26];
 
-        StringBuilder stack = new StringBuilder();
 
-        for(int i = 0 ; i<s.length();i++){
-             char ch = s.charAt(i);
-             if(visited[ch-'a']){
-                  continue;
+         for(int i = 0 ; i<s.length() ;i++){
+                lastIndex[s.charAt(i)-'a'] = i ;
+         }
+          StringBuilder sb = new StringBuilder();
+
+         for(int i = 0 ; i<s.length() ; i++){
+               char ch= s.charAt(i);
+             if(visited[ch - 'a']){
+                  continue ;
              }
-             while(!stack.isEmpty()&& stack.charAt(stack.length()-1)>ch && lastIndex[stack.charAt(stack.length()-1)-'a']>i){
-                 visited[stack.charAt(stack.length()-1)-'a'] = false ;
-                 stack.deleteCharAt(stack.length()-1);
+             while(!sb.isEmpty() && sb.charAt(sb.length()-1)>ch&& lastIndex[sb.charAt(sb.length()-1)-'a']>i){
+                  visited[sb.charAt(sb.length()-1)-'a'] = false ;
+                  sb.deleteCharAt(sb.length()-1);
              }
-             stack.append(ch);
+             sb.append(ch);
              visited[ch-'a'] = true ;
-        }
-      return stack.toString();
 
+         }
 
+         return sb.toString();
     }
 }
