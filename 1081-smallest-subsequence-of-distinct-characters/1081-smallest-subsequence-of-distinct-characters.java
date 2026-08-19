0a58902921fbef7c2c9c99cfd4840e70e2921e85
Ankit Saini <ankiitsaini1403@gmail.com>
2026-08-19 06:53:27 +0530

    Time: 2 ms (96.65%), Space: 42.7 MB (90.68%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,32 +1,31 @@
 class Solution {
     public String smallestSubsequence(String s) {
-        int[] lastIndex = new int[26];
-         boolean[]  visited = new boolean[26];
+        int lastIndex[] = new int[26];
+        boolean visited[] = new boolean[26];
 
-         for(int i =0 ;i<s.length();i++){
-               lastIndex[s.charAt(i)-'a']= i ;
-         }
+        for(int i = 0 ; i<s.length() ; i++){
+              lastIndex[s.charAt(i)-'a'] = i ;
+        }
 
-         StringBuilder stack = new StringBuilder();
 
-         for(int i = 0 ;i<s.length();i++){
-             char ch = s.charAt(i);
+        StringBuilder stack = new StringBuilder();
 
-             if(visited[ch-'a']){
+        for(int i =0  ; i<s.length() ; i++){
+              char ch = s.charAt(i);
+              if(visited[ch-'a']){
                   continue ;
-             }
-             while(!stack.isEmpty() && stack.charAt(stack.length()-1)>ch && lastIndex[stack.charAt(stack.length()-1)-'a']>i){
-                      visited[stack.charAt(stack.length()-1 )-'a'] = false ;
-                      stack.deleteCharAt(stack.length()-1);
-                      
-                     }
+              }
 
+              while(!stack.isEmpty() && stack.charAt(stack.length()-1)>ch &&
+              lastIndex[stack.charAt(stack.length()-1)-'a']>i ){
+                   visited[stack.charAt(stack.length()-1)-'a'] = false ;
+                  stack.deleteCharAt(stack.length()-1);
+              }
+              
+              stack.append(ch);
+              visited[ch-'a'] = true ;
+        }
 
-             stack.append(ch);
-             visited[ch-'a'] = true ;
-
-         }
-
-         return stack.toString();
+        return stack.toString();
     }
 }
