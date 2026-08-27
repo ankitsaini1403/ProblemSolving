6591044916c348dc636f3e490ad999a490a06df2
Ankit Saini <ankiitsaini1403@gmail.com>
2026-08-27 16:26:09 +0530

    Time: 5 ms (98.23%), Space: 42.6 MB (95.6%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,27 +1,26 @@
 class Solution {
     public int countSubstrings(String s) {
-        
+        int count = 0;
 
-        int n = s.length(); 
-        int count =0;
-
-        for(int i= 0 ;i<n ;i++){
-             for(int j =i ;j <n ; j++){
-                 if(isPalindrome(s, i , j)){
-                     count++;
-                 }
-             }
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);       // odd length
+            count += expand(s, i, i + 1);   // even length
         }
 
-        return count ;
+        return count;
     }
 
-    public boolean isPalindrome(String s, int left , int right){
-         while(left<right){
-              if(s.charAt(left) != s.charAt(right)) return false ;
-              left++;
-              right--;
-         }
-         return true ;
+    private int expand(String s, int left, int right) {
+        int count = 0;
+
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+
+            count++;
+            left--;
+            right++;
+        }
+
+        return count;
     }
 }
