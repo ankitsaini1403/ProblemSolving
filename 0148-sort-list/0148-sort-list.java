f8d420794b269439276adeb30292a05a12e4ca2e
Ankit Saini <ankiitsaini1403@gmail.com>
2026-09-11 09:58:42 +0530

    Time: 9 ms (96.38%), Space: 59.3 MB (62.15%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,55 +1,57 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode sortList(ListNode head) {
-         if (head == null || head.next == null) {
-            return head;
-        }
+        if(head ==null || head.next == null) return head ;
+         
+         ListNode slow = head ;
+         ListNode fast = head ;
 
-        ListNode slow = head ;
-        ListNode fast = head ;
-
-        while(fast.next != null  && fast.next.next != null){
-              slow= slow.next ;
+         while(fast.next != null && fast.next.next != null){
+              slow =slow.next ;
               fast = fast.next.next ;
-        }
-  
-       ListNode head1 = slow.next ;
-       slow.next = null ;
+         }
 
-       head = sortList(head);
-       head1 = sortList(head1);
-       
-       return merge(head, head1);
+         ListNode head1 = slow.next ;
+         slow.next= null ;
 
+       head =  sortList(head);
+        head1 = sortList(head1);
+
+         return merge(head, head1);
+        
 
     }
 
-    public ListNode merge(ListNode head,ListNode head1){
-            ListNode dummy = new ListNode(0);
-            
-            ListNode temp = dummy ;
-            ListNode  temp1 = head ;
-            ListNode temp2 = head1 ;
+    public static ListNode merge(ListNode head , ListNode head1){
+             ListNode dummy = new ListNode(0);
+             ListNode temp = dummy;
+             ListNode temp1= head ;
+             ListNode temp2 = head1 ;
 
-            while(temp1 != null && temp2!= null){
-                 if(temp1.val<=temp2.val){
-                      temp.next = temp1;
-                      temp1 =temp1.next ;
-                 }
-                 else {
-                      temp.next = temp2 ;
-                      temp2 =temp2.next ;
-                 }
-                 temp = temp.next ;
-            }
+             while(temp1!=null && temp2 != null){
+                   if(temp1.val <= temp2.val){
+                       temp.next = temp1;
+                       temp1= temp1.next ;
+                   }
+                   else {
+                          temp.next = temp2 ;
+                          temp2 =temp2.next;
+                   }
+                   temp = temp.next;
+             }
 
-            if(temp1 != null){
-                 temp.next = temp1 ;
-            }
-            else {
-                  temp.next = temp2 ;
-            }
+             if(temp1 != null) temp.next = temp1 ;
+             else  temp.next = temp2 ;
 
-            return dummy.next ;
+             return dummy.next ;
     }
 }
