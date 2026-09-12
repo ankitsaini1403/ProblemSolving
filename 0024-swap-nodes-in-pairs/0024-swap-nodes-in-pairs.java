b1533ddbffb76c22b6485c5a44a72408471a4bd3
Ankit Saini <ankiitsaini1403@gmail.com>
2026-09-12 20:08:37 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.95%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -1,28 +1,57 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
-       
-       
-        
-        
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        
-    
-        while (prev.next != null && prev.next.next != null) {
-           
-            ListNode first = prev.next;
-            ListNode second = first.next;
-            
-           
-            first.next = second.next;
-            second.next = first;
-            prev.next = second;
-            
-          
-            prev = first;
+
+        if (head == null || head.next == null) {
+            return head;
         }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+       
+        while (even != null && even.next != null) {
+
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+
+        odd.next = null;
+
+        if (even != null) {
+            even.next = null;
+        }
+
         
-        return dummy.next;
+        ListNode d1 = new ListNode(0);
+        ListNode temp = d1;
+
+        ListNode temp1 = evenHead;
+        ListNode temp2 = head;
+
+        while (temp1 != null && temp2 != null) {
+
+            temp.next = temp1;
+            temp1 = temp1.next;
+            temp = temp.next;
+
+            temp.next = temp2;
+            temp2 = temp2.next;
+            temp = temp.next;
+        }
+
+        if (temp1 != null) {
+            temp.next = temp1;
+        }
+
+        if (temp2 != null) {
+            temp.next = temp2;
+        }
+
+        return d1.next;
     }
 }
