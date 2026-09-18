f8eb788aaf0ad623307a95b9c6807f6cfe41aa9a
Ankit Saini <ankiitsaini1403@gmail.com>
2026-09-18 07:01:20 +0530

    Time: 56 ms (40.64%), Space: 135 MB (24.97%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+         ListNode curr = head ;
+         Stack<ListNode> stack = new Stack<>();
+
+         while(curr!= null){
+             while(!stack.isEmpty() && stack.peek().val < curr.val){
+                    stack.pop();
+             }
+             stack.push(curr);
+             curr = curr.next ;
+         }
+
+         ListNode dummy = new ListNode(0);
+         ListNode tail = dummy ;
+
+         for(ListNode node : stack){
+              tail.next = node ;
+              tail = tail.next ;
+         }
+         tail.next = null ;
+
+         return dummy.next ;
+
+    }
+}
