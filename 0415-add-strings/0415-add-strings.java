510b07bae09d85068819e7de022a8c1534008f8b
Ankit Saini <ankiitsaini1403@gmail.com>
2026-09-23 04:45:57 +0530

    Time: 2 ms (94.8%), Space: 44.1 MB (27.6%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length()-1 ;
+        int j = num2.length()-1 ;
+        StringBuilder sb = new StringBuilder();
+        int carry = 0 ;
+        int sum =0 ;
+        while(i>=0 || j>= 0 || carry!=0){
+             sum = carry ;
+
+           int digit1 = i>=0 ? num1.charAt(i)-'0':0;
+           int digit2 = j>=0 ? num2.charAt(j)-'0' : 0;
+
+           sum+= digit1+digit2 ; 
+           sb.append(sum%10);
+           carry = sum/10 ;
+           i--;
+           j--;
+
+        }
+
+        sb.reverse();
+
+        return sb.toString();
+    
+
+    }
+}
